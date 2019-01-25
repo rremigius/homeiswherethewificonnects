4f03fe34e0d81242e067c35da9ed9b44137a6e0a
Bram <bramtimmer89@gmail.com>
2019-01-25 15:20:53 -0800

    added material for BalancePlatform added material for PlayerColor added PlayerController movement script

---
+++ Assets/Game/Scenes/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 18;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal") * speed;
+        float vAxis = Input.GetAxis("Vertical") * speed;
+
+        transform.Translate(hAxis * Time.deltaTime, 0, vAxis * Time.deltaTime);
+    }
+}
