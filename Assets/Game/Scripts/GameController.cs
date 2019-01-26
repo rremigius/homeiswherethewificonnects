7fcbe0a6d6eed31c48308f4f54aaf9a6e2f83917
ESTHER\Esther <estherzuidgeest@hotmail.com>
2019-01-26 14:41:38 +0100

    removed player color and start position arrays Added a reference to the taskmanager

--- Assets/Game/Scripts/GameController.cs
+++ Assets/Game/Scripts/GameController.cs
@@ -6,17 +6,14 @@ using UnityEngine.Assertions;
 public class GameController : MonoBehaviour
 {
     public int numberOfPlayers = 4;
-
     public Spawner playerSpawner;
-
-    private Color[] PlayerColors = { Color.green, Color.magenta, Color.yellow, Color.blue };
-    private Vector3[] StartPositions = { new Vector3(0, 1, 0), new Vector3(1, 0, 0), new Vector3(-1, 0, 0), new Vector3(0, -1, 0) };
-
+    private TaskManager MyTaskManager;
 
     void Start()
     {
         Assert.IsNotNull(playerSpawner);
-        playerSpawner.Spawn(numberOfPlayers);
+        var Players = playerSpawner.Spawn(numberOfPlayers);
+        MyTaskManager = GetComponent<TaskManager>();
     }
 
 
