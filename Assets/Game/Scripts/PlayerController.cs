74e1431ebb5a308dbcf8abea7f4fb3333b160d47
ESTHER\Esther <estherzuidgeest@hotmail.com>
2019-01-26 13:38:36 +0100

    Voeg lock en unlock en kleur toe aan player character. De game controller spawned characters de task manager gaat straks taken uitdelen aan spelers

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 18;
+    public Color PlayerColor = new Color(1, 0, 0, 1);
     public bool IsBusy { get; private set; }  = false;
     public void LockPlayer() { IsBusy = true; }
     public void UnlockPlayer() { IsBusy = false; }
+    
 
     // Update is called once per frame
     void Update()
@@ -23,6 +25,9 @@ public class PlayerController : MonoBehaviour
         transform.Translate(hAxis * Time.deltaTime, 0, vAxis * Time.deltaTime);
     }
 
-
+    void SetPlayerColor(Color newColor)
+    {
+        GetComponent<MeshRenderer>().material.SetColor("_Color", newColor);
+    }
 
 }
