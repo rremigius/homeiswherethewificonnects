5a6218cac241c610f39daa07903ae9eef53c26b5
ESTHER\Esther <estherzuidgeest@hotmail.com>
2019-01-26 12:33:06 +0100

    Added a playerCharacter prefab with collision UPdated interactive object which now locks the player for a 1 second duration and can highlight and unhighlight

--- Assets/Game/Scripts/InteractiveObject.cs
+++ Assets/Game/Scripts/InteractiveObject.cs
@@ -10,37 +10,78 @@ public class InteractiveObject : MonoBehaviour
     public delegate void ActionSuccess();
     public static event ActionSuccess BroadCastSuccess;
 
-    public bool IsOccupied { get; private set; }
-    public float TaskDuration;
+    public bool IsOccupied { get; private set; } = false;
+    public float TaskDuration = 1;
+    public float Points = 10;
+    
 
-    InteractiveObject() // set defaults
-    {
-        IsOccupied = false;
-    }
+    private PlayerController PC;
+    private float TargetTime;
+    private bool TimerActive = false;
+    private Color UnhighlightedColor = new Color(0, 0, 0, 0);
+
     void Start()
     {
         //TODO check if box component available of requirement/required component bovenaan class
     }
-    void Update() { }
-
-    //when player enters trigger, check if its the correct player
-    private void OnTriggerEnter(Collider other)
+    void Update()
     {
-        if (IsColliderAssignedPlayer(other))
-        {
-            IsOccupied = true;
-            //Activate minigame
-            //If Succesfull
-            IsOccupied = false;
-            BroadCastSuccess();
+        if (TimerActive && Time.time >= TargetTime) {
+                TimerActive = false;
+                TaskCompleted();
         }
+        
     }
 
+    //Arms this interactive object with a char reference and a highlight, it can now be overlapped with
+    public void AssignTask(PlayerController Player,Color PlayerColor)
+    {
+        assignedPlayer = Player;
+        SetColor(PlayerColor);
+    }
+
+    //when player enters trigger, check if its the correct player and if so start task
+    private void OnTriggerEnter(Collider other)
+    {
+        if (IsColliderAssignedPlayer(other) && IsOccupied==false)
+        {
+            PC = other.gameObject.GetComponent<PlayerController>();
+            StartTask();
+        }
+    }
     private bool IsColliderAssignedPlayer(Collider other)
     {
         return assignedPlayer == other.gameObject.GetComponent<PlayerController>();
     }
 
-    
 
+    void StartTask()
+    {
+        IsOccupied = true;
+        PC.LockPlayer();
+        
+        //Activate minigame
+        TargetTime = Time.time + TaskDuration;
+        TimerActive = true;
+        
+    }
+
+    void TaskCompleted()
+    {
+        PC.UnlockPlayer();
+        BroadCastSuccess();
+        Reset();
+    }
+
+    void Reset()
+    {
+        assignedPlayer = null;
+        IsOccupied = false;
+        SetColor(UnhighlightedColor);
+    }
+
+    void SetColor(Color newColor)
+    {
+        GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", newColor);
+    }
 }
--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -4,13 +4,25 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 18;
+    public bool IsBusy { get; private set; }  = false;
+    public void LockPlayer() { IsBusy = true; }
+    public void UnlockPlayer() { IsBusy = false; }
 
     // Update is called once per frame
     void Update()
+    {
+        if (IsBusy == false) { MoveCharacter(); }
+        
+    }
+
+    void MoveCharacter()
     {
         float hAxis = Input.GetAxis("Horizontal") * speed;
         float vAxis = Input.GetAxis("Vertical") * speed;
 
         transform.Translate(hAxis * Time.deltaTime, 0, vAxis * Time.deltaTime);
     }
+
+
+
 }
