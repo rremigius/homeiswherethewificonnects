8eb2a82c52c2e7478c937f9312ff88f35b59610f
Remi <remialkemade@gmail.com>
2019-01-26 14:40:15 +0100

    Changed Spawner.Spawn method

--- Assets/Game/Scripts/Spawner.cs
+++ Assets/Game/Scripts/Spawner.cs
@@ -6,11 +6,7 @@ public class Spawner : MonoBehaviour
 {
     public List<GameObject> prefabs;
     public List<Transform> locations;
-
-    public bool randomizePrefabs = false;
-    public bool randomizeLocations = false;
-
-    public Vector3 randomLocationArea;
+    public Transform parent;
 
     int next = 0;
 
@@ -26,12 +22,34 @@ public class Spawner : MonoBehaviour
         
     }
 
-    public void Spawn() {
+    GameObject CreateNewObject() {
+        int objIndex = next % prefabs.Count;
+        return Instantiate(prefabs[objIndex]);
+    }
+
+    Transform GetNewLocation() {
+        int locationIndex = next % locations.Count;
+        return locations[locationIndex];
+    }
+
+    protected virtual void AfterSpawn(GameObject gameObject, int index) {}
+
+    public List<GameObject> Spawn(int count = 1) {
         if(prefabs.Count == 0) {
             Debug.LogError("Cannot spawn. No prefabs specified.");
-            return;
+            return null;
         }
-        next = next % prefabs.Count;
-        GameObject newObj = Instantiate(prefabs[next]);
+        
+        List<GameObject> spawned = new List<GameObject>();
+        for(int i = 0; i < count; i++) {
+            GameObject newObj = CreateNewObject();
+            Transform location = GetNewLocation();
+            newObj.transform.parent = parent;
+            newObj.transform.position = location.position;
+
+            spawned.Add(newObj);
+        }
+
+        return spawned;
     }
 }
