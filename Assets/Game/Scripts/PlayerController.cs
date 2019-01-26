0bde031b17e36eaea7782c0b1483b273bbf19e4f
ESTHER\Esther <estherzuidgeest@hotmail.com>
2019-01-26 13:46:26 +0100

    removed merge texts

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -5,21 +5,18 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 18;
     public Color PlayerColor = new Color(1, 0, 0, 1);
-<<<<<<< HEAD
     public int inputID = 1;
-=======
     public bool IsBusy { get; private set; }  = false;
     public void LockPlayer() { IsBusy = true; }
     public void UnlockPlayer() { IsBusy = false; }
->>>>>>> 2df4210e3579babb8ce98cdef3ce37cd1cb09cb6
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
+
         float hAxis = Input.GetAxis("Horizontal" + inputID) * speed;
         float vAxis = -Input.GetAxis("Vertical" + inputID) * speed;
-=======
+
         if (IsBusy == false) { MoveCharacter(); }
         
     }
@@ -28,7 +25,7 @@ public class PlayerController : MonoBehaviour
     {
         float hAxis = Input.GetAxis("Horizontal") * speed;
         float vAxis = Input.GetAxis("Vertical") * speed;
->>>>>>> 2df4210e3579babb8ce98cdef3ce37cd1cb09cb6
+
 
         transform.Translate(hAxis * Time.deltaTime, 0, vAxis * Time.deltaTime);
     }
