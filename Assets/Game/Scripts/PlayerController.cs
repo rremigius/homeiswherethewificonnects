c09d4ad348561aee265f873e232d2cdff6929add
Remi <remialkemade@gmail.com>
2019-01-26 13:33:26 +0100

    Players are controlled by gamepads (player 1 and 2 for now both on gamepad 1).

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -4,12 +4,13 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 18;
+    public int inputID = 1;
 
     // Update is called once per frame
     void Update()
     {
-        float hAxis = Input.GetAxis("Horizontal") * speed;
-        float vAxis = Input.GetAxis("Vertical") * speed;
+        float hAxis = Input.GetAxis("Horizontal" + inputID) * speed;
+        float vAxis = -Input.GetAxis("Vertical" + inputID) * speed;
 
         transform.Translate(hAxis * Time.deltaTime, 0, vAxis * Time.deltaTime);
     }
