af57066528dc0c800d0fc05ffe4f2f5701759de0
ESTHER\Esther <estherzuidgeest@hotmail.com>
2019-01-26 10:20:21 +0100

    Added my scene to test things in because someone submitted an empty scene over the main

---
+++ Assets/Game/Scripts/BalancePlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BalancePlatform : MonoBehaviour
+{
+    Rigidbody body;
+
+    public float maxTilt = 45;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Constrain();
+    }
+
+    void Constrain() {
+        // float torqueX = transform.eulerAngles.x * 10;
+        // float torqueZ = transform.eulerAngles.z * 10;
+        // body.AddTorque(new Vector3(-torqueX, 0, torqueZ));
+    }
+}
