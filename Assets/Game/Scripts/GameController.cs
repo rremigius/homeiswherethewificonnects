35a7b22ea6ed8bb3a086e2fddd59921f8a85cf18
Remi <remialkemade@gmail.com>
2019-01-26 14:21:43 +0100

    Moved spawning from GameController to Spawner

--- Assets/Game/Scripts/GameController.cs
+++ Assets/Game/Scripts/GameController.cs
@@ -1,23 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class GameController : MonoBehaviour
 {
     public int numberOfPlayers = 4;
+
+    public Spawner playerSpawner;
+
     private Color[] PlayerColors = { Color.green, Color.magenta, Color.yellow, Color.blue };
     private Vector3[] StartPositions = { new Vector3(0, 1, 0), new Vector3(1, 0, 0), new Vector3(-1, 0, 0), new Vector3(0, -1, 0) };
-    //allows you to put a ref to player prefab in the gamecontroller
-    public GameObject PlayerCharacterPrefab;
 
 
     void Start()
     {
-        for (int p = 0; p < numberOfPlayers; p++)
-        {
-            GameObject PlayerCharacter = Instantiate(PlayerCharacterPrefab);
-            PlayerCharacter.GetComponent<PlayerController>().PlayerColor = PlayerColors[p];
-            //PlayerCharacter.transform.position = StartPositions[p];
+        Assert.IsNotNull(playerSpawner);
+
+        for (int p = 0; p < numberOfPlayers; p++) {
+            playerSpawner.Spawn();
         }
     }
 
---
+++ Assets/Game/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> prefabs;
+    public List<Transform> locations;
+
+    public bool randomizePrefabs = false;
+    public bool randomizeLocations = false;
+
+    public Vector3 randomLocationArea;
+
+    int next = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawn() {
+        if(prefabs.Count == 0) {
+            Debug.LogError("Cannot spawn. No prefabs specified.");
+            return;
+        }
+        next = next % prefabs.Count;
+        GameObject newObj = Instantiate(prefabs[next]);
+    }
+}
