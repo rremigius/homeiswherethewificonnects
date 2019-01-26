6d35d5c840afe41323f7c994e59fa3ef4bbc04ee
ESTHER\Esther <estherzuidgeest@hotmail.com>
2019-01-26 20:07:33 +0100

    IO is nu default kleur doorzichtig Ik heb de standaard player kleuren veranderd

--- Assets/Game/Scripts/PlayerSpawner.cs
+++ Assets/Game/Scripts/PlayerSpawner.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayerSpawner : Spawner<PlayerController>
 {
-    public List<Color> colors = new List<Color>();
-    public Color[] colors2 = { Color.yellow, Color.green, Color.magenta, Color.blue };
+    //public List<Color> colors = new List<Color>();
+    public Color[] colors2;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (colors2.Length==0)
+        {
+            colors2 = new Color[] { Color.yellow, Color.red, Color.magenta, Color.cyan };
+        }
     }
 
     // Update is called once per frame
