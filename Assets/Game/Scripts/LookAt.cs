a3a8cca170977241203d28ca1810cf20c3281574
Remi <remialkemade@gmail.com>
2019-01-27 12:51:15 +0100

    Added LookAt for sprites

---
+++ Assets/Game/Scripts/LookAt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public string lookAtTag = "PlayerCamera";
+
+    GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("PlayerCamera");
+        if(target == null) {
+            Debug.LogError("No object found with tag " + lookAtTag + ". I cannot stare at something if I don't know what.");
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target.transform);
+    }
+}
